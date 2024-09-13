9b0999233039e1ec220e8620ad6a1d945b280c93
Youcef Goudjal <48559920+Youcef-Goudjal@users.noreply.github.com>
2024-09-13 15:49:31 +0100

    Question preview- name

--- frontend/faciquest/lib/features/survey/view/questions/previews/audio_record_question_preview.dart
+++ frontend/faciquest/lib/features/survey/view/questions/previews/audio_record_question_preview.dart
@@ -10,6 +10,6 @@ class AudioRecordQuestionPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Text('Coming soon');
   }
 }
--- frontend/faciquest/lib/features/survey/view/questions/previews/name_question_preview.dart
+++ frontend/faciquest/lib/features/survey/view/questions/previews/name_question_preview.dart
@@ -1,3 +1,6 @@
+import 'package:awesome_extensions/awesome_extensions.dart';
+import 'package:faciquest/core/context_extension.dart';
+import 'package:faciquest/core/core.dart';
 import 'package:faciquest/features/features.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +13,35 @@ class NameQuestionPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        if (question.showFirstName) ...[
+          TextField(
+            decoration: InputDecoration(
+              labelText: question.firstNameLabel,
+              hintText: question.firstNameHint,
+            ),
+          ),
+        ],
+        if (question.showLastName) ...[
+          AppSpacing.spacing_1.heightBox,
+          TextField(
+            decoration: InputDecoration(
+              labelText: question.lastNameLabel,
+              hintText: question.lastNameHint,
+            ),
+          ),
+        ],
+        if (question.showMiddleName) ...[
+          AppSpacing.spacing_1.heightBox,
+          TextField(
+            decoration: InputDecoration(
+              labelText: question.middleNameLabel,
+              hintText: question.middleNameHint,
+            ),
+          ),
+        ]
+      ],
+    );
   }
 }
