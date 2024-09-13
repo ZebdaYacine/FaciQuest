c30e6c86cd608f049a400f00b8704046969426f7
Youcef Goudjal <48559920+Youcef-Goudjal@users.noreply.github.com>
2024-09-13 10:53:09 +0100

    date time preview

--- frontend/faciquest/lib/features/survey/view/questions/previews/date_time_question_preview.dart
+++ frontend/faciquest/lib/features/survey/view/questions/previews/date_time_question_preview.dart
@@ -1,7 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:faciquest/features/features.dart';
 import 'package:flutter/material.dart';
 
-class DateTimeQuestionPreview extends StatelessWidget {
+class DateTimeQuestionPreview extends StatefulWidget {
   const DateTimeQuestionPreview({
     required this.question,
     super.key,
@@ -9,7 +10,36 @@ class DateTimeQuestionPreview extends StatelessWidget {
   final DateTimeQuestion question;
 
   @override
+  State<DateTimeQuestionPreview> createState() =>
+      _DateTimeQuestionPreviewState();
+}
+
+class _DateTimeQuestionPreviewState extends State<DateTimeQuestionPreview> {
+  final controller = TextEditingController();
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextField(
+      controller: controller,
+      onTap: () async {
+        final date = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(1900),
+          lastDate: DateTime(2100),
+        );
+        if (date != null) {
+          controller.text = DateFormat('dd/MM/yyyy').format(date);
+        }
+      },
+      decoration: const InputDecoration(
+          // hintText: question.hint,
+          ),
+    );
   }
 }
