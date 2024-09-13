d2b3271e5357c4edcb2b37642b60eb65828473aa
Youcef Goudjal <48559920+Youcef-Goudjal@users.noreply.github.com>
2024-09-13 10:59:43 +0100

    Question preview- phone number

--- frontend/faciquest/lib/features/survey/view/questions/previews/phone_question_preview.dart
+++ frontend/faciquest/lib/features/survey/view/questions/previews/phone_question_preview.dart
@@ -1,5 +1,6 @@
 import 'package:faciquest/features/features.dart';
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class PhoneQuestionPreview extends StatelessWidget {
   const PhoneQuestionPreview({
@@ -10,6 +11,15 @@ class PhoneQuestionPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return IntlPhoneField(
+      decoration: const InputDecoration(
+        labelText: 'Phone Number',
+        border: OutlineInputBorder(
+          borderSide: BorderSide(),
+        ),
+      ),
+      initialCountryCode: 'DZ',
+      onChanged: (phone) {},
+    );
   }
 }
