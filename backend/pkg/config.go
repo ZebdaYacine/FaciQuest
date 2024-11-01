5dc10260f6d889ae366e4866b4539ecec7895d52
ZebdaYacine <36519629+ZebdaYacine@users.noreply.github.com>
2024-11-01 22:07:34 +0100

    nn

--- backend/pkg/config.go
+++ backend/pkg/config.go
@@ -12,7 +12,7 @@ import (
 func LoadEnv() {
 	err := godotenv.Load("../.env")
 	if err != nil {
-		log.Fatal("Error loading ..env file")
+		log.Fatal("========= Error loading ..env file")
 	}
 	godotenv.Load(".env")
 }
