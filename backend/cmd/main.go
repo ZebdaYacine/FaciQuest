95ba68cf308527c2896e3c51bfad241f59a597d2
ZebdaYacine <36519629+ZebdaYacine@users.noreply.github.com>
2024-11-02 10:46:23 +0100

    delete redis server

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -142,20 +142,19 @@ package main
 
 import (
 	"back-end/api/server"
-	"back-end/cache"
 	common "back-end/core"
 	"back-end/pkg/database"
 	"log"
 )
 
 func main() {
-	redis, err := cache.CheckRedisConnection()
-	if err != nil {
-		panic(err)
-	}
+	// redis, err := cache.CheckRedisConnection()
+	// if err != nil {
+	// 	panic(err)
+	// }
 	// cache.AddKey(rdb, "sed", "opfsdpofdsf", 100*time.Second)
 	db := database.ConnectionDb()
-	server.InitGinServer(db, redis)
+	server.InitGinServer(db, nil)
 	log.Printf("STRINGING SERVER %s", common.RootServer.SECRET_KEY)
 
 }
